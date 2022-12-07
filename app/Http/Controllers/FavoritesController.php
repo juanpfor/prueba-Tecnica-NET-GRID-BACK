36f74bcd6ceafd6d5dc2179da9ac57cbbab1a7fa
Nonimus <jpceron0@misena.edu.co>
2022-12-07 15:45:36 -0600

    create migrations

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\favorites;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\favorites  $favorites
+     * @return \Illuminate\Http\Response
+     */
+    public function show(favorites $favorites)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\favorites  $favorites
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(favorites $favorites)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\favorites  $favorites
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, favorites $favorites)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\favorites  $favorites
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(favorites $favorites)
+    {
+        //
+    }
+}
